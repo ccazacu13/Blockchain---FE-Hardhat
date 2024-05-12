50cc2dee3b74daa076863f782740ecc8502ec1f7
Cristian Cazacu <cristiancazacu076@gmail.com>
2024-05-12 21:58:40 +0300

    Get Local Blockchain up and get test accounts

--- src/Connect.js
+++ src/Connect.js
@@ -1,27 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Web3 from 'web3';
 import Test from './Test';
 
 export default function Connect(){
 
-    const connectToMetaMask = async () => {
-        if (window.ethereum) {
-            window.web3 = new Web3(window.ethereum);
-            try {
-                await window.ethereum.enable();
-            } catch (error) {
-                console.error("User denied account access");
-            }
-        } else {
-            console.error("MetaMask not detected");
-        }
+    const [web3, setWeb3] = useState();
+
+    const connectToLocalNetwork = async () => {
+        const web3 = new Web3('http://localhost:8545');
+        setWeb3(web3);
     };
 
     useEffect(() =>{
-        connectToMetaMask();
+        connectToLocalNetwork();
     }, []);
 
     return(
-        <Test />
+        <Test web3={web3}/>
     )
 }
--- src/Test.js
+++ src/Test.js
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
 
-export default function Test(){
+export default function Test({web3}){
 
     const [accounts, setAccounts] = useState([]);
 
     useEffect(() => {
 
         const getAccounts = async () => {
-            const accounts = await window.web3.eth.getAccounts();
+            const accounts = await web3.eth.getAccounts();
             setAccounts(accounts);
         }
 
         getAccounts();
     
-    }, [])
+    }, [web3])
 
     return(
         <>
             <div>This is a test.</div>
             <div>
-                {accounts.map((account) => <><div>{account}</div><br /></>)}
+                {accounts ? accounts.map((account) => <><div>{account}</div><br /></>) : <div>Yet to load</div>}
             </div>
         </>
     )

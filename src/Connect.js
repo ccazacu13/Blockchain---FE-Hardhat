cf23f19e70ee6bcdf9c850ba104070df26126e37
Cristian Cazacu <cristiancazacu076@gmail.com>
2024-05-12 21:17:31 +0300

    Connect Via Metamask

---
+++ src/Connect.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import Web3 from 'web3';
+
+export default function Connect(){
+
+    const connectToMetaMask = async () => {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            try {
+                await window.ethereum.enable();
+            } catch (error) {
+                console.error("User denied account access");
+            }
+        } else {
+            console.error("MetaMask not detected");
+        }
+    };
+
+    useEffect(() =>{
+        connectToMetaMask();
+    }, []);
+
+    return(
+        <div>Ceva</div>
+    )
+}

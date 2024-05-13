09015785deea6323c9f091bc48c35d615b844f60
Cristian Cazacu <cristiancazacu076@gmail.com>
2024-05-13 18:19:22 +0300

    HardHat Setup for Metamask

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,4 +2,12 @@
 
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: 'state hour chalk envelope naive obscure caught frog drink giraffe relief prosper',
+      },
+      chainId: 1337,
+    },
+  },
 };
